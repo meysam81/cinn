1983af9127fd88ca2b57e962d1d2f53273b6d522
Meysam Azad <MeysamAzad81@yahoo.com>
2018-09-22 18:19:38 +0330

    add types and header in include

--- include/cinn.hrl
+++ include/cinn.hrl
@@ -3,6 +3,11 @@
 -ifndef(HEADER_CINN).
 -define(HEADER_CINN, true).
 
+-type hostname() :: list().
+-type key() :: list().
+-type value() :: term().
+
+-define(ZABBIX_HEADER, <<"ZBXD", 1:8/integer>>).
 
 -ifdef(TEST).
 -define(LOG_ERROR(Format, Args), ct:print(default, 50, Format, Args)).
